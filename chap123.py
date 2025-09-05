1e553afe5a18a4e8081fe6e4afcf0217dacfea6e
mingyu0605 <163095459+mingyu0605@users.noreply.github.com>
2025-09-05 15:29:08 +0900

    Add files via upload

--- chap123.py
+++ chap123.py
@@ -8,10 +8,9 @@ img = cv2.line(img,(0,0),(511,511),(255,0,0),5)
 img = cv2.rectangle(img(384,0),(510,128),(0,255,0),3)
 img = cv2.circle(img(447,63), 63, (0,0.255),30)
 img = cv2.ellipse(img(256,256),(100,50),0,0,180,255,-1)
+font = cv2.FONT_HERSHEY_SIMPLEX
+cv2.putText(img,'OpenCV',(10,500), font, 8,(255,255,255),12,cv2.LINE_AA)
 cv2.imshow('frame',img)
-k = cv2.waitKey(0)
-if cv2.waitKey(1) & 0xFF == ord('q'):
-    cv2.destroyALLWindows()
 k = cv2.waitKey(0)
 if k == 27: # wait for 's' key to save and exit
     cv2.destroyAllWindows()
